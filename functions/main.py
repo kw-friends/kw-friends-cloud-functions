86d44641216301a06b72a4c3b355e48dd821a5a7
Seungman Lim <woori4158@gmail.com>
2024-02-28 14:26:56 +0900

    first commit

---
+++ functions/main.py
@@ -0,0 +1,34 @@
+# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+from firebase_functions import db_fn
+
+# The Firebase Admin SDK to access the Firebase Realtime Database.
+from firebase_admin import initialize_app, db
+
+# The Firebase Admin SDK to access Cloud Firestore.
+from firebase_admin import firestore, messaging
+
+app = initialize_app()
+
+#registration_token = 'token'
+
+@db_fn.on_value_created(
+    reference = r"/chattings/messages/{roomID}/{messageID}",
+    region = "asia-southeast1"
+)
+def onMessageCreated(event: db_fn.Event[db_fn.Change]):
+
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title = event.data['uid'],
+            body = event.data['content'],
+        ),
+        token=registration_token,
+    )
+
+    # Send a message to the device corresponding to the provided
+    # registration token.
+    response = messaging.send(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
+
+    
